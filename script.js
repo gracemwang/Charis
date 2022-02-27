fadd04b06ba9f22c54fcf139e8b03187b89e5a58
gracemwang <grace.mingbo.wang@gmail.com>
2022-02-26 19:45:26 -0800

    all files

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-function toggleDisplay(id) {
-    document.getElementById(id).style.visibility = 'visible';
+function show(id) {
+  document.getElementById(id).style.visibility = 'visible';
+}
+
+function hideAll() {
+  document.getElementById('sd').style.visibility = 'hidden';
+  document.getElementById('tr').style.visibility = 'hidden';
+  document.getElementById('ny').style.visibility = 'hidden';
+  document.getElementById('tk').style.visibility = 'hidden';
+  document.getElementById('d').style.visibility = 'hidden';
+}
+
+function city(){
+  const city = document.getElementById('bar').value.toLowerCase();
+  hideAll();
+  if (city === "san diego") {
+    show('sd');
   }
+  else if (city === "toronto") {
+    show('tr');
+  }
+  else if (city === "new york city") {
+    show('ny');
+  }
+  else if (city === "tokyo") {
+    show('tk');
+  }
+  else if (city === "new delhi") {
+    show('d');
+  }
+  else {
+    alert("We don't have volunteer opportunities for the city \"" + city + "\" yet, try another city!");
+  }
+}
+
